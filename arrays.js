15dc5fb09709e27df96899253951e0b4aa614231
Nora Gaughan <noragaughan@gmail.com>
2020-07-13 15:18:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,3 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-var addElementToBeginningOfArray = [1, 'two', 3, 'four']
-addElementToBeginningOfArray.unshift('five')
+var 
 
